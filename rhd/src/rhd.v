77ace45d39c3e8ba2d1f2033cb1444ee5cda96e2
Hao Le <51278143+anhhao135@users.noreply.github.com>
2024-02-03 17:31:00 -0800

    Update rhd.v

--- rhd/src/rhd.v
+++ rhd/src/rhd.v
@@ -914,8 +914,7 @@ module rhd
                             end
                             else if ((INTAN_reg != INTAN_expected || INTAN_DDR_reg != INTAN_expected) && flag_cable_delay_low_found) begin
                                 state_cable_delay_finder = DONE; 
-                                //phase_select = (phase_select_low + phase_select) / 2;
-                                phase_select = delay_A;
+                                phase_select = (phase_select_low + phase_select) / 2;
                             end
                             else begin
                                 phase_select = phase_select + 1;
