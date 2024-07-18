3d8da6b62a97f96feb9c58dfbeabc3c753887204
Hao Le <anhhao135@gmail.com>
2024-07-18 09:44:10 -0700

    Revert "stim always on bug fix"
    This reverts commit 82aeb284a10ac424979a9e29e9a8ad1075cdc97c.

--- rhs/src/rhs.v
+++ rhs/src/rhs.v
@@ -2794,8 +2794,7 @@ module command_selector_stim (
 			15:      MOSI_cmd <= { 1'b0, 2'b00, 2'b01, DSP_settle, 4'b0000, channel, 16'h0000 };
 			16:		 MOSI_cmd <= stim_en ? { 2'b10, 2'b00, 4'b0000, stim_on_register, stim_on_cmd  }            : ZCheck_cmd_1; // turn stim on/off
 			17:		 MOSI_cmd <= stim_en ? { 2'b10, 2'b00, 4'b0000, stim_pol_register, stim_pol_cmd }           : ZCheck_cmd_2; // set stim polarity , 1 is pos, 0 is neg
-            18:		 MOSI_cmd <= !stim_en ? { 2'b10, 2'b00, 4'b0000, stim_on_register, 0} : read_rom255_cmd; // when stim_en is low, always turn off the stimulators; else just do dummy command
-			//18:		 MOSI_cmd <= { 2'b11, 2'b00, 4'b0000, compliance_register, 16'h0000 }; //            : read_rom255_cmd;
+			18:		 MOSI_cmd <= { 2'b11, 2'b00, 4'b0000, compliance_register, 16'h0000 }; //            : read_rom255_cmd;
 			19:		 MOSI_cmd <= stim_en ? { 2'b10, 2'b11, 4'b0000, charge_recov_register, charge_recov_cmd }   : read_rom255_cmd; // set charge recovery, trigger U flag
 			default: MOSI_cmd <= 32'b0;
 			endcase
